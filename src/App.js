9d7095192c339b7f3a1f5b664d51486343f6efaa
pratikrk <83352530+pratikrk@users.noreply.github.com>
2023-05-01 11:22:48 +0530

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
-// import { Home }from './components/homepage/Home';
-import { Home, Room, FindRoomMate, Signup, LandingPage, Login, EditProfile} from './components';
+import { useState } from 'react';
+import { Home, Room, FindRoomMate, Signup, LandingPage, Login, EditProfile } from './components';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Nav from './components/navbar/Nav';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // set initial value to false
+
+  // function to check if user is logged in
+  const checkAuth = () => {
+    if (isLoggedIn) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <>
-    <Nav />
-    <Routes>
-        <Route path="/" element={<LandingPage/>} />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/editprofile" element={<EditProfile />} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/findroom" element={<Room/>} />
-        <Route path="/findroommate" element={<FindRoomMate/>} />
+        
+        {/* Protected routes */}
+        <Route path="/editprofile" element={checkAuth() ? <EditProfile /> : <Navigate to="/login" />} />
+        <Route path="/home" element={checkAuth() ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/findroom" element={checkAuth() ? <Room /> : <Navigate to="/login" />} />
+        <Route path="/findroommate" element={checkAuth() ? <FindRoomMate /> : <Navigate to="/login" />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+      </Routes>
     </>
   );
 }
